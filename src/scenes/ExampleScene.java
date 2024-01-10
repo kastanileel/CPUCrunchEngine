e25c38901e271792ffd79e8b0db3acbc2c308b3d
Marcel <marcelflashinc@gmail.com>
2024-01-10 09:44:06 +0100

    add UV mapping and Texturing for AKM, Sniper and Shotgun

--- src/scenes/ExampleScene.java
+++ src/scenes/ExampleScene.java
@@ -49,7 +49,7 @@ public class ExampleScene extends Scene {
 
             id = manager.createEntity(GameComponents.TRANSFORM | GameComponents.RENDER | GameComponents.PICKUPWEAPON);
             if (id > -1) {
-                manager.rendering[id].mesh = new Mesh("src/objects/misc/healthPack.obj", "src/objects/misc/healthPack128.png");//"./src/objects/rock/rock64.png");
+                manager.rendering[id].mesh = new Mesh("src/objects/guns/shotgun/superShotgun.obj", "src/objects/guns/shotgun/Shotgun128.png");//"./src/objects/rock/rock64.png");
                 manager.rendering[id].renderType = GameComponents.Rendering.RenderType.Textured;
                 manager.transform[id].pos = new Vector3(-0.5f, - 0.1f, 10.0f);
                 manager.transform[id].rot = new Vector3(0.0f, 0.0f, 0.0f);
@@ -139,7 +139,7 @@ public class ExampleScene extends Scene {
 
                 manager.playerMovement[id].weaponType = GameComponents.PlayerMovement.WeaponType.MACHINE_GUN;
 
-                manager.rendering[id].mesh = new Mesh("./src/objects/guns/machineGun/AKM.obj", "./src/objects/guns/machineGun/AKM_texture.png");
+                manager.rendering[id].mesh = new Mesh("./src/objects/guns/machineGun/AKM.obj", "./src/objects/guns/machineGun/AKM128.png");
                 manager.rendering[id].renderType = GameComponents.Rendering.RenderType.Textured;
                 manager.rendering[id].modelTranslation = new Vector3(-0.5f, -0.5f, 3.0f);
                 manager.rendering[id].modelRotation = new Vector3(0.0f, 0.0f, 0.0f);
@@ -178,7 +178,7 @@ public class ExampleScene extends Scene {
             }*/
 
            /*id = manager.createEntity(GameComponents.TRANSFORM | GameComponents.PLAYERMOVEMENT
-                    | GameComponents.PHYSICSBODY | GameComponents.RENDER | GameComponents.COLLIDER);
+                    | GameComponents.PHYSICSBODY | GameComponents.RENDER | GameComponents.COLLIDER| GameComponents.DAMAGEABLE);
             if (id > -1){
                 manager.transform[id].pos = new Vector3(0.0f, 0.0f, 0.0f);
                 manager.transform[id].rot = new Vector3(0.0f, 0.0f, 0.0f);
@@ -191,8 +191,8 @@ public class ExampleScene extends Scene {
 
                 manager.playerMovement[id].weaponType = GameComponents.PlayerMovement.WeaponType.SNIPER;
 
-                manager.rendering[id].mesh = new Mesh("./src/objects/guns/sniper/AWP.obj", Color.GRAY);
-                manager.rendering[id].renderType = GameComponents.Rendering.RenderType.OneColor;
+                manager.rendering[id].mesh = new Mesh("./src/objects/guns/sniper/AWP.obj", "./src/objects/guns/sniper/Sniper128.png");
+                manager.rendering[id].renderType = GameComponents.Rendering.RenderType.Textured;
                 manager.rendering[id].modelTranslation = new Vector3(-0.5f, -0.7f, 4.2f);
                 manager.rendering[id].modelRotation = new Vector3(0.0f, 0.0f, 0.0f);
 
